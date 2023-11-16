f97110003fe49d625240f3ce11b6afe3cff72f92
Kipkerich <jkikerich@kabarak.ac.ke>
2023-11-16 16:28:00 +0300

    First commit

---
+++ shushanPalaceHotel/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return  render(request,'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+
+def menu(request):
+    return render(request, 'menu.html')
+
+
+
